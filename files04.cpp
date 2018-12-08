49d1a000223b3166f547abada3e6c8af098423bf
Sean <xsmpx09@hotmail.com>
2018-12-08 00:10:05 -0500

    updates...

--- files04.cpp
+++ files04.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+    char letter;
+
+    letter = 'A';
+    cout << letter << endl;
+
+    letter = 'B';
+    cout << letter << endl;
+
+    letter = 65;
+    cout << letter << endl;
+
+    letter = 66;
+    cout << letter << endl;
+
+    return 0;
+
+}
